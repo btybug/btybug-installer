8009ee83b997e3c8607e78252a396d3748fd8134
sahakavatar <hakobyan.sahak88@gmail.com>
2017-11-02 21:29:04 +0400

    update with /btybug/container/archive/master.zip

--- public/functions.php
+++ public/functions.php
@@ -56,7 +56,7 @@ function requirements($args)
 }
 function download(){
     $fh =__DIR__."/master.zip";
-//    $url='https://github.com//sahakavatar/bootydev/archive/master.zip';
+    $url='https://github.com//btybug/container/archive/master.zip';
     $url='http://dev.bootydev.co.uk/public/bootydev.zip';
     $result=file_put_contents($fh, fopen($url, 'r'));
     $data=['success'=>$result];
